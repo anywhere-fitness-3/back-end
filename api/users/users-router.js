fb63adfc22ced20355f04d7679f59b03f1f3cbb1
bdrummo6 <bradley.drummonds@gmail.com>
2020-07-27 17:53:33 -0400

    Update users-router.js

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -41,7 +41,7 @@ router.post('/register', async (req, res, next) => {
 	try {
 		const { first_name, last_name, email, username, password, role_id } = req.body;
 		const user = await Users.findBy({ username }).first();
-		const email = await Users.findBy({ email }).first();
+		const mail = await Users.findBy({ email }).first();
 
 		if (user) {
 			return res.status(409).json({
@@ -49,7 +49,7 @@ router.post('/register', async (req, res, next) => {
 			});
 		}
 
-		if (email) {
+		if (mail) {
 			return res.status(409).json({
 				message: 'Email is already taken',
 			});
