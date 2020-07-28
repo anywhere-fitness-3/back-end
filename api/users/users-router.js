0185ea6579cc7218cfb8dcf36dd340a7b2484e52
bdrummo6 <bradley.drummonds@gmail.com>
2020-07-28 18:33:43 -0400

    Update users-router.js

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -2,12 +2,11 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const Users = require('./users-model');
-const restrict = require('../../middleware/restrict');
 
 const router = express.Router();
 
 // Retrieve all users
-router.get('/', restrict(0), async (req, res, next) => {
+router.get('/', async (req, res, next) => {
 	try {
 		const users  = await Users.findAll();
 		res.json(users);
@@ -32,76 +31,6 @@ router.get('/roles/:role_id', async (req, res, next) => {
 		const users  = await Users.findByRole(req.params.role_id);
 		res.json(users);
 	} catch (err) {
-		next(err);
-	}
-})
-
-// Creates a new user in the database
-router.post('/register', async (req, res, next) => {
-	try {
-		const { first_name, last_name, email, username, password, role_id } = req.body;
-		const user = await Users.findBy({ username }).first();
-		const mail = await Users.findBy({ email }).first();
-
-		if (user) {
-			return res.status(409).json({
-				message: 'Username is already taken',
-			});
-		}
-
-		if (mail) {
-			return res.status(409).json({
-				message: 'Email is already taken',
-			});
-		}
-
-		const newUser = await Users.add({
-			first_name,
-			last_name,
-			email,
-			username,
-			password: await bcrypt.hashSync(password, 8),
-			role_id
-		})
-
-		res.status(201).json(newUser);
-	} catch(err) {
-		next(err);
-	}
-})
-
-// Creates a login session for a user
-router.post('/login', async (req, res, next) => {
-	try {
-		const { username, password } = req.body;
-		const user = await Users.findBy({ username }).first();
-
-		if (!user) {
-			return res.status(401).json({
-				message: 'You shall not pass!',
-			});
-		}
-
-		const passwordValid = await bcrypt.compareSync(password, user.password);
-
-		if (!passwordValid) {
-			return res.status(401).json({
-				message: 'You shall not pass!',
-			});
-		}
-
-		const payload = {
-			id: user.id,
-			username: user.username,
-			role_id: user.role_id,
-		}
-
-		res.cookie('token', jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h'}));
-
-		res.json({
-			message: `Welcome ${user.username}!`,
-		});
-	} catch(err) {
 		next(err);
 	}
 })
