397b0857e38e0a7d1c588f829531be17ee5958df
bdrummo6 <bradley.drummonds@gmail.com>
2020-07-28 19:54:09 -0400

    Update server.js

--- server.js
+++ server.js
@@ -15,8 +15,14 @@ server.use(cookieParser());
 server.use(express.json());
 
 server.use(logger('long'));
+<<<<<<< Updated upstream
 server.use('/users', usersRouter);
 server.use('/classes', classesRouter);
+=======
+server.use('/api', welcomeRouter);
+server.use('/api/users', usersRouter);
+server.use('/api/classes', classesRouter);
+>>>>>>> Stashed changes
 
 server.get('/', (req, res) => {
 	res.status(200).json({
