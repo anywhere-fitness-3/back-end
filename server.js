a8d39b2a6d76eab9118ecd0e9b329532ef37614c
bdrummo6 <bradley.drummonds@gmail.com>
2020-07-28 19:57:32 -0400

    Updates

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const logger = require('./middleware/logger')
 
+const welcomeRouter = require('./api/welcome/welcome-router.js');
 const usersRouter = require('./api/users/users-router.js');
 const classesRouter = require('./api/classes/classes-router.js');
 
@@ -15,18 +16,13 @@ server.use(cookieParser());
 server.use(express.json());
 
 server.use(logger('long'));
-<<<<<<< Updated upstream
-server.use('/users', usersRouter);
-server.use('/classes', classesRouter);
-=======
 server.use('/api', welcomeRouter);
 server.use('/api/users', usersRouter);
 server.use('/api/classes', classesRouter);
->>>>>>> Stashed changes
 
 server.get('/', (req, res) => {
 	res.status(200).json({
-		message: 'The API for Anywhere Fitness is running!'
+		message: 'The API is running!'
 	});
 });
 
