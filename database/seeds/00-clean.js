0906fb9202cc4c0ff5280e1c79aadec814f1fa73
bdrummo6 <bradley.drummonds@gmail.com>
2020-07-29 17:12:45 -0400

    Delete 00-clean.js

--- database/seeds/00-clean.js
+++
@@ -1,8 +0,0 @@
-
-exports.seed = async function(knex) {
-   await knex('class_clients').truncate();
-   await knex('classes').truncate();
-   await knex('types').truncate();
-   await knex('users').truncate();
-   await knex('roles').truncate();
-}
