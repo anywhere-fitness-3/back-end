b8e94fd4f1daaa719debb02606f4aa12fe0f79c6
bdrummo6 <bradley.drummonds@gmail.com>
2020-07-30 15:57:39 -0400

    Update classes-router.js

--- api/classes/classes-router.js
+++ api/classes/classes-router.js
@@ -5,9 +5,10 @@ const router = express.Router();
 // Retrieve all classes
 router.get('/', async (req, res, next) => {
 	try {
-		const courses  = await Classes.findAll();
-		res.json(courses);
-	} catch (err) {
+		const newClass = req.body;
+		Classes.add(newClass);
+		res.status(201).json(newClass);
+	} catch(err) {
 		next(err);
 	}
 })
