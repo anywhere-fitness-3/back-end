bc621b2d2bd018b99c354f1d72e4badc39019a27
bdrummo6 <bradley.drummonds@gmail.com>
2020-07-29 23:21:38 -0400

    Changes add function

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -41,25 +41,14 @@ function findBy(filter) {
 	return db('users').where(filter);
 }
 
-/* // Adds a user object to the database
+// Adds a user object to the database
 async function add(user) {
-	await db('users').insert(user);
+	const [id] = await db('users').insert(user);
 
-	return db('users').join('roles', 'users.role_id', '=', 'roles.id')
-		.select('users.id as user_id', 'users.first_name', 'users.last_name', 'users.email',
-			'users.username', 'roles.role')
-		.where('users.id').first();
-} */
+	return findById(id);
+}
 
-function add(user) {
-	return db('users')
-	  .insert(user, 'id')
-	  .then(ids => {
-		// console.log(ids);
-		return findById(ids[0]);
-	  });
-  }
-  
+
 
 // Updates a current user with the specified changes
 async function update(id, changes) {
