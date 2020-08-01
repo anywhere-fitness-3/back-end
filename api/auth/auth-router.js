7f5b1249c0bf7d09c0bf9b166d78c9bc87748f6d
bdrummo6 <bradley.drummonds@gmail.com>
2020-08-01 14:56:03 -0400

    Update auth-router.js

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+require("dotenv").config();
 const Users = require('../users/users-model');
 
 const router = express.Router();
