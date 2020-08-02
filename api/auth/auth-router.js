1436393874a14fdfbae2aed330b48f2c8a5cdad6
bdrummo6 <bradley.drummonds@gmail.com>
2020-08-02 13:56:24 -0400

    Adjusted secret in auth-router

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const JWT_SECRET = "wib! FtZS I6Ik pvaG*";
+const JWT_SECRET = "this is a secret";
 const Users = require('../users/users-model');
 
 const router = express.Router();
