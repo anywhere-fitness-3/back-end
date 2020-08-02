57f8a1b72671baebc66ffec76dc68fd45524a43a
Bradley Drummonds <47236494+bdrummo6@users.noreply.github.com>
2020-08-02 02:47:04 -0400

    Update server.js

--- server.js
+++ server.js
@@ -11,12 +11,17 @@ const authRouter = require('./api/auth/auth-router');
 const usersRouter = require('./api/users/users-router.js');
 const classesRouter = require('./api/classes/classes-router.js');
 
-// CORS
-// This allows client applications from other domains use the API Server
-server.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+      return res.status(200).json({});
+  }
+  return next();
 });
 
 server.use(helmet());
