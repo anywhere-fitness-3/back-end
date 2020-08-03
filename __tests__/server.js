13c0629dac1069f3d0251d3bad1674122f696934
bdrummo6 <bradley.drummonds@gmail.com>
2020-08-03 18:11:15 -0400

    Updated tests in server test suite

--- __tests__/server.js
+++ __tests__/server.js
@@ -1,6 +1,5 @@
 const request = require('supertest');
 const server = require('../server');
-const db = require('../database/dbConfig');
 
 describe('testing for endpoints', () => {
 	it('GET /', async () => {
@@ -17,20 +16,6 @@ describe('testing for endpoints', () => {
 		// `content-type` headers tell the client how to render the data
 		expect(res.headers['content-type']).toBe('application/json; charset=utf-8')
 		expect(res.body.message).toBe('Welcome to the Anywhere Fitness API!')
-	})
-
-	it('GET /api/classes', async () => {
-		const res = await request(server).get('/api/classes')
-		expect(res.statusCode).toBe(200)
-		expect(res.headers["content-type"]).toBe("application/json; charset=utf-8")
-		expect(res.body).toHaveLength(6)
-	})
-
-	it('GET /api/users', async () => {
-		const res = await request(server).get('/api/users')
-		expect(res.statusCode).toBe(200)
-		expect(res.headers["content-type"]).toBe("application/json; charset=utf-8")
-		expect(res.body).toHaveLength(15)
 	})
 
 })
